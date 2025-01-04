6afbcb50532839c83e590c62a76306f950bc4ed3
BimadityaR <bimaaditya0711@gmail.com>
2025-01-04 22:24:54 +0700

    menambahkan pada user controller update profile dan crud pada reviews sisi pengguna

--- TUBESWEB/app/Http/Controllers/UserController.php
+++ TUBESWEB/app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Models\City;
 use App\Models\Facilities;
 use App\Models\gallary;
 use App\Models\Property;
+use App\Models\Reviews;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\UserData;
@@ -111,6 +112,19 @@ class UserController extends Controller
         return view('User.Profile', $data);
     }
     
+    //edit profile Page
+    public function edituserprofile(Request $request)
+    {
+        $user = $request->session()->get('user');
+        $userId = $user['id'];
+        $user = User::with('Data')->findOrFail($userId);
+        $title = $user->name . " | Edit Profile";
+        $menu = "none";
+
+        $data = compact('title', 'menu', 'user');
+        return view('User.editProfile', $data);
+    }
+
     public function editeduserprofile(Request $request)
     {
         $user = $request->session()->get('user');
@@ -192,6 +206,263 @@ class UserController extends Controller
         $request->session()->forget('user');
 
         return redirect()->back();
+    }
+
+    //sending home
+    public function userHome(Request $request)
+    {
+        $title = "Home";
+        $menu = "home";
+        $featuredPro = Property::with('Cate', 'City')
+            ->where('public', true)
+            ->where('featured', true)
+            ->latest()->limit(4)->get();
+        $newlyAdded = Property::with('Cate', 'City')
+            ->where('public', true)
+            ->latest()->limit(6)->get();
+        $showcate = Category::latest()->limit(6)->get();
+        $catedata = Category::with('Pro')->latest()
+            ->get();
+
+        $data = compact('title', 'menu', 'featuredPro', 'newlyAdded', 'showcate', 'catedata');
+        return view('frontend.home', $data);
+    }
+    
+    //Showing Properties
+    public function show(Request $request)
+    {
+        $show = Property::with('Cate', 'City')
+            ->where('public', true)
+            ->latest()
+            ->paginate(10);
+        $title = 'Properties';
+        $menu = 'none';
+
+        $data = compact('title', 'menu', 'show');
+        if ($request->ajax()) {
+            return view('frontend.showinitem', compact('show'));
+        } else {
+            return view('frontend.show', $data);
+        }
+    }
+
+    public function show_category(Request $request)
+    {
+        $valid = validator($request->route()->parameters(), [
+            'cate' => 'exists:categories,slug_name'
+        ])->validate();
+        $cate_fltr = $request->route()->parameter('cate');
+        $cate = Category::where('slug_name', '=', $cate_fltr)->first();
+        $show = Property::with('Cate', 'City')
+            ->where('public', true)
+            ->where('category', '=', $cate->id)
+            ->latest()
+            ->paginate(10);
+        $title = $cate->name;
+        $menu = 'category';
+
+        $data = compact('title', 'menu', 'show', 'cate_fltr');
+        return view('frontend.show', $data);
+    }
+
+    public function show_city(Request $request)
+    {
+        $valid = validator($request->route()->parameters(), [
+            'city' => 'exists:cities,slug_city'
+        ])->validate();
+        $city_fltr = $request->route()->parameter('city');
+        $city = City::where('slug_city', '=', $city_fltr)->first();
+        $show = Property::with('Cate', 'City')
+            ->where('public', true)
+            ->where('city', '=', $city->id)
+            ->latest()
+            ->paginate(10);
+        $title = $city->city;
+        $menu = 'city';
+
+        $data = compact('title', 'menu', 'show', 'city_fltr');
+        return view('frontend.show', $data);
+    }
+
+    public function show_purpose(Request $request)
+    {
+        $purpose_fltr = $request->route()->parameter('purpose');
+        $show = Property::with('Cate', 'City')
+            ->where('public', true)
+            ->where('purpose', '=', $purpose_fltr)
+            ->latest()
+            ->paginate(10);
+        $title = ucfirst($purpose_fltr);
+        $menu = 'purpose';
+        $data = compact('title', 'menu', 'show', 'purpose_fltr');
+        return view('frontend.show', $data);
+    }
+
+    public function show_pro(Request $request)
+    {
+        $valid = validator($request->route()->parameters(), [
+            'pro' => 'exists:properties,title_slug'
+        ])->validate();
+        $pro = $request->route()->parameter('pro');
+        $item = Property::with('Cate', 'City')
+            ->where('title_slug', '=', $pro)
+            ->first();
+        $faci = [];
+        $facis = json_decode($item->faci);
+        if (!empty($facis)) {
+            foreach ($facis as $key => $value) {
+                $faci[$key] = Facilities::where('faci', '=', $value)->first() ?? null;
+            }
+        }
+        $gals = gallary::with('Pro')->where('pro_id', '=', $item->id)->get();
+        $userId = $request->session()->get('user')['id'] ?? null;
+        $user_reviews = Reviews::where('u_id', $userId)->where('pro_id', $item->id)->latest()->get();
+        $reviews = Reviews::with('Users')
+            ->where('u_id', '!=', $userId)->where('pro_id', $item->id)
+            ->latest()
+            ->limit(10)
+            ->get();
+        $title = $item->title;
+        $menu = 'none';
+        $data = compact('title', 'menu', 'item', 'gals', 'faci', 'user_reviews', 'reviews');
+        return view('frontend.property', $data);
+    }
+    
+    //filter Property
+    public function ajaxFilter(Request $request)
+    {
+        if ($request->ajax()) {
+            $cate = $request->category;
+            if ($cate == '*') {
+                $cateS = ['category', '!=', null];
+            } else {
+                $cate = Category::where('slug_name', '=', $cate)->first();
+                $cateS = ['category', '=', $cate->id];
+            }
+            $city = $request->city;
+            if ($city == '*') {
+                $cityS = ['city',  '!=', null];
+            } else {
+                $city = City::where('slug_city', '=', $city)->first();
+                $cityS = ['city', '=', $city->id];
+            }
+            $purpose = $request->purpose;
+            if ($purpose == '*') {
+                $purposeS = ['purpose',  '!=', null];
+            } else {
+                $purposeS = ['purpose', '=', $purpose];
+            }
+            $search = stripslashes(strip_tags($request->search));
+            $searchStr = ['title', 'LIKE', '%' . $search . '%'];
+            $sort = $request->sort;
+            switch ($sort) {
+                case 'latest':
+                    $sortW = 'created_at';
+                    $sortS = 'desc';
+                    break;
+                case 'oldest':
+                    $sortW = 'created_at';
+                    $sortS = 'asc';
+                    break;
+                case 'phtl':
+                    $sortW = 'price';
+                    $sortS = 'desc';
+                    break;
+                case 'plth':
+                    $sortW = 'price';
+                    $sortS = 'asc';
+                    break;
+                case 'ahtl':
+                    $sortW = 'area';
+                    $sortS = 'desc';
+                    break;
+                case 'alth':
+                    $sortW = 'area';
+                    $sortS = 'asc';
+                    break;
+            }
+
+            $show = Property::with('Cate', 'City')
+                ->where('public', true)
+                ->where([
+                    $cateS,
+                    $cityS,
+                    $purposeS,
+                    $searchStr,
+                ])
+                ->orderBy($sortW, $sortS)
+                ->paginate(10);
+
+            return view('frontend.showinitem', compact('show'));
+        }
+    }
+
+    //search Property
+    public function propSearch(Request $request)
+    {
+        $request->validate([
+            'purpose' => 'required',
+        ]);
+
+        $SecStr = stripslashes(strip_tags($request->search));
+        $searchStr = '%' . $SecStr . '%';
+        $purpose = $request->purpose;
+        if ($purpose == '*') {
+            $purposeS = ['purpose', '!=', null];
+        } else {
+            $purposeS = ['purpose', '=', $purpose];
+        }
+        $show = Property::with('Cate', 'City')
+            ->where('public', true)
+            ->where([
+                $purposeS,
+                ['title', 'LIKE', $searchStr]
+            ])
+            ->latest()
+            ->paginate(10);
+        $title = 'Propeties';
+        $menu = 'none';
+
+        $data = compact('title', 'menu', 'show', 'SecStr', 'purpose');
+        return view('frontend.show', $data);
+    }
+    
+
+
+    //adding property review using ajax
+    public function add_review(Request $request)
+    {
+        if ($request->ajax()) {
+            $request->validate([
+                'review_text' => 'required'
+            ]);
+            $userId = $request->session()->get('user')['id'];
+            $review = new Reviews;
+            $review->u_id = $request->u_id;
+            $review->pro_id = $request->pro_id;
+            $review->review = $request->review_text;
+            $review->save();
+            $user = User::with('Data')->findOrFail($userId);
+            $request->session()->put('user', $user->toArray());
+
+            $res = ['status' => true, 'review' => $review];
+            return json_encode($res, true);
+        }
+    }
+
+    //deleting review with ajax
+    public function del_review($id, Request $request)
+    {
+        if ($request->ajax()) {
+            $userId = $request->session()->get('user')['id'];
+            $review = Reviews::FindOrFail($id);
+            $review->delete();
+            $user = User::with('Data')->findOrFail($userId);
+            $request->session()->put('user', $user->toArray());
+
+            $res = ['status' => true];
+            return json_encode($res, true);
+        }
     }
 
     
